f654671eae3c1a4cd0e65a7ae9148e48015abb32
Ángel Tejeda <angel.tejedat@gmail.com>
2021-10-27 00:49:53 -0500

    E2 A3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { VistasComponent } from './components/vistas/vistas.component';
+import { ListComponent } from './components/list/list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    VistasComponent
+    VistasComponent,
+    ListComponent
   ],
   imports: [
     BrowserModule
